7f05d1229e934404703edf0915b95adfbda8301a
msardic <msungurardic@gmail.com>
2019-07-04 20:57:43 +0300

    4 tane bastı

--- js/character-detail.js
+++ js/character-detail.js
@@ -18,11 +18,9 @@ fetch('https://swapi.co/api/people/'+parsedData[1]+'/?format=json')
     }).then(function(returnData2){
       console.log(returnData2.title)
       
-})
-});
- 
 
-  
+
+
 
   swData.insertAdjacentHTML('afterend',
 
@@ -37,7 +35,7 @@ fetch('https://swapi.co/api/people/'+parsedData[1]+'/?format=json')
 '<tr><th>BIRTH YEAR</td><td class="birthYear">'+returnData.birth_year+'</td></tr>'+
 '<tr><th>GENDER</td><td class="gender">'+returnData.gender+'</td></tr>'+
 '<tr><th>HOMEWORLD</td><td class="homeworld">'+returnData.homeworld+'</td></tr>'+
-'<tr><th>FILMS</td><td id="films">'+returnData.films.reduce((total,item)=>total + (" "+item),"")+'</td></tr>'+
+'<tr><th>FILMS</td><td id="films">'+returnData2.title+'</td></tr>'+
 '<tr><th>SPECIES</td><td class="species">'+returnData.species.reduce((total,item)=>total + (" "+item),"")+'</td></tr>'+
 '<tr><th>VEHICLES</td><td class="vehicles">'+returnData.vehicles.reduce((total,item)=>total + (" "+item),"")+'</td></tr>'+
 '<tr><th>STARSHIPS</td><td class="starships">'+returnData.starships.reduce((total,item)=>total + (" "+item),"")+'</td></tr>'+
@@ -46,6 +44,9 @@ fetch('https://swapi.co/api/people/'+parsedData[1]+'/?format=json')
 );
 
 
+
+})
+});
 
 
 
